6cb9351ae05bb4ae173dcb67edbb2ef1bf490f72
Zack Will <1278408180@qq.com>
2024-02-06 15:26:00 +0800

    Port ivshmem.h

--- include/ivshmem-client.h
+++ include/ivshmem-client.h
@@ -22,7 +22,7 @@
 #include <sys/select.h>
 
 #include "qemu/queue.h"
-#include "hw/misc/ivshmem.h"
+#include "qemu/ivshmem.h"
 
 /**
  * Maximum number of notification vectors supported by the client
