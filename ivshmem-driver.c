a05659b193e572f82e08b51102c675907fbb03a9
Zack Will <1278408180@qq.com>
2024-03-14 15:55:12 +0800

    Finish driver code

--- ivshmem-driver.c
+++ ivshmem-driver.c
@@ -1,50 +1,111 @@
 #include <uk/config.h>
 #include <uk/print.h>
+#include <uk/plat/common/cpu.h>
 #include <uk/arch/types.h>
-#include <uk/alloc.h>
 #include <uk/intctlr.h>
-#include <uk/bus/pci.h>
+#include "./include/ivshmem-driver.h"
 
-static struct uk_alloc *a;
-
-#define IVSHMEM_PCI_VENDOR_ID (0x1Af4)
-#define IVSHMEM_PCI_DEVICE_ID (0x1110)
-#define IVSHMEM_REVISION_NUM  1
-#define DRV_NAME	          "ivshmem_driver"
-#define IVSHMEM_REGISTER_BAR  0
-#define IVSHMEM_MEMORY_BAR    2
-
-struct ivshmem_pci_dev {
-    
-    __u64 pci_base_addr;
-
-    struct pci_device *pdev;
-};
+struct ivshmem_pci_dev *ivshmem_dev;
 
 /**
  * Static function declaration.
  */
-static int ivshmem_pci_add_dev(struct pci_device *pci_dev);
-static int ivshmem_pci_drv_init(struct uk_alloc *drv_allocator);
 
+static inline uint32_t get_config_addr(uint32_t bus, uint32_t device, uint32_t function, uint32_t offset)
+{
+    return (PCI_ENABLE_BIT)
+			| (bus << PCI_BUS_SHIFT)
+			| (device << PCI_DEVICE_SHIFT)
+			| (function << PCI_FUNCTION_SHIFT)
+            | (offset);
+}
+/**
+ * Read pci config data from addr
+*/
+static inline uint32_t pci_conf_read(uint32_t addr)
+{
+    outl(PCI_CONFIG_ADDR, addr);
+    return inl(PCI_CONFIG_DATA);
+}
+/**
+ * Write data to addr
+*/
+static inline void pci_conf_write(uint32_t addr, uint32_t data)
+{
+    outl(PCI_CONFIG_ADDR, addr);
+	outl(PCI_CONFIG_DATA, data);
+    return;
+}
 
-static int ivshmem_pci_add_dev(struct pci_device *pci_dev){
-    struct ivshmem_pci_dev *ipci_dev = NULL;
-    int rc = 0;
-
+static int ivshmem_pci_add_dev(struct pci_device *pci_dev)
+{
     UK_ASSERT(pci_dev != NULL);
+    int rc = 0;
+    uint32_t bus, device, function, command_addr, command_data;
+    uint32_t bar0_addr, bar1_addr, bar2_addr, bar3_addr;
+    uint32_t bar0_data, bar1_data, bar2_data, bar3_data;
+
+    pci_dev->state = PCI_DEVICE_STATE_RUNNING;
+
     uk_pr_info("Allocate ivshmem device\n");
-    ipci_dev = uk_malloc(a, sizeof(*ipci_dev));
-    if (!ipci_dev) {
+    ivshmem_dev = uk_malloc(a, sizeof(*ivshmem_dev));
+    if (!ivshmem_dev) {
         uk_pr_err("Failed to allocate ivshmem pci device\n");
         return -ENOMEM;
     }
 
-    ipci_dev->pdev = pci_dev;
-    ipci_dev->pci_base_addr = pci_dev->base;
+    bus = pci_dev->addr.bus;
+    device = pci_dev->addr.devid;
+    function = pci_dev->addr.function;
+
+    /* Enable memory and IO */
+    command_addr = get_config_addr(bus, device, function, PCI_COMMAND);
+    pci_conf_write(command_addr, PCI_COMMAND_DECODE_ENABLE | PCI_COMMAND_MASTER);
+    command_data = pci_conf_read(command_addr);
+    uk_pr_info("command_data:0x%lx\n", command_data);
+
+    /* Probe BAR data */
+    bar0_addr = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_0);
+    pci_conf_write(bar0_addr, 0xffffffff);
+    bar0_data = pci_conf_read(bar0_addr);
+
+    bar2_addr = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_2);
+    pci_conf_write(bar2_addr, 0xffffffff);
+	bar2_data = pci_conf_read(bar2_addr);
+    uk_pr_info("bar2_data:0x%lx\n", bar2_data);
+
+    bar3_addr = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_3);
+    pci_conf_write(bar3_addr, 0xffffffff);
+	bar3_data = pci_conf_read(bar3_addr);
+    uk_pr_info("bar3_data:0x%lx\n", bar3_data);
+
+    /* Calculate share memory region size */
+    uint64_t ivshmem_size = ~((((uint64_t)bar3_data << 32) | bar2_data) & 0xfffffffffffffff0) + 1;
+    uk_pr_info("ivshmem_size:0x%lx\n", ivshmem_size);
+    /* Allocate memory for shm region */
+    void *mem_addr = uk_memalign(a, ivshmem_size, ivshmem_size);
+    if(!mem_addr) {
+        uk_pr_err("Failed to allocate BAR region\n");
+        return -ENOMEM;
+    }
+    uk_pr_info("mem_addr_start:0x%lx\n", mem_addr);
+    /* Write memory adress back to BAR*/
+    bar2_data = (uint64_t)mem_addr & 0xffffffff;
+    bar3_data = (uint64_t)mem_addr >> 32;
+    uk_pr_info("bar2_data:0x%lx\n", bar2_data);
+    uk_pr_info("bar3_data:0x%lx\n", bar3_data);
+    pci_conf_write(bar3_addr, bar3_data);
+    pci_conf_write(bar2_addr, bar2_data);
+
+    ivshmem_dev->pdev = pci_dev;
+    ivshmem_dev->ivshmem_size = ivshmem_size;
+    ivshmem_dev->ivshmem_addr_start = mem_addr;   
+    
+    return 0;
 }
 
-static int ivshmem_pci_drv_init(struct uk_alloc *drv_allocator){
+static int ivshmem_pci_drv_init(struct uk_alloc *drv_allocator)
+{
     if (!drv_allocator)
         return -EINVAL;
     uk_pr_info("Initialize ivshmem driver\n");
@@ -54,7 +115,7 @@ static int ivshmem_pci_drv_init(struct uk_alloc *drv_allocator){
 
 static const struct pci_device_id ivshmem_pci_ids[] = {
     {PCI_DEVICE_ID(IVSHMEM_PCI_VENDOR_ID, IVSHMEM_PCI_DEVICE_ID)},
-    {PCI_ANY_DEVICE_ID}
+    {PCI_ANY_DEVICE_ID},
 };
 
 static struct pci_driver ivshmem_pci_drv = {
