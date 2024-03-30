f8325e356a27c5fbaab8acbb8405b0ef7b3b33de
Zack Will <wczhou@zju.edu.cn>
2024-03-30 15:12:37 +0800

    Add devfs support

--- ivshmem-driver.c
+++ ivshmem-driver.c
@@ -3,9 +3,18 @@
 #include <uk/plat/common/cpu.h>
 #include <uk/arch/types.h>
 #include <uk/intctlr.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <uk/essentials.h>
+#include <vfscore/uio.h>
+#include <devfs/device.h>
 #include "./include/ivshmem-driver.h"
 
+#define DEV_IVSHMEM_NAME "ivshmem"
+
 struct ivshmem_pci_dev *ivshmem_dev;
+
+
 
 /**
  * Static function declaration.
@@ -42,8 +51,8 @@ static int ivshmem_pci_add_dev(struct pci_device *pci_dev)
     UK_ASSERT(pci_dev != NULL);
     int rc = 0;
     __u32 bus, device, function, command_addr, command_data;
-    __u32 bar0_addr, bar1_addr, bar2_addr, bar3_addr;
-    __u32 bar0_data, bar1_data, bar2_data, bar3_data;
+    __u32 devreg_addr, bar1_data, bar2_data, bar3_data;
+    __u32 int_mask, int_stat, IVPosition, doorbell;
 
     pci_dev->state = PCI_DEVICE_STATE_RUNNING;
 
@@ -65,15 +74,28 @@ static int ivshmem_pci_add_dev(struct pci_device *pci_dev)
     uk_pr_info("command_data:0x%lx\n", command_data);
 
     /* Probe BAR data */
-    bar0_addr = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_0);
-    bar0_data = pci_conf_read(bar0_addr);
+    ivshmem_dev->bar_addr[0] = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_0);
+    devreg_addr = pci_conf_read(ivshmem_dev->bar_addr[0]);
+    uk_pr_info("bar0_data:0x%lx\n", devreg_addr);
     
-    bar2_addr = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_2);
-	bar2_data = pci_conf_read(bar2_addr);
+    /* Probe interrupt info */
+    int_mask = readl(devreg_addr);
+    uk_pr_info("interrupt mask:0x%lx\n", int_mask);
+    int_stat = readl(devreg_addr | 0x4);
+    uk_pr_info("interrupt status:0x%lx\n", int_stat);
+    IVPosition = readl(devreg_addr | 0x8);
+    uk_pr_info("IVPosition:0x%lx\n", IVPosition);
+
+    ivshmem_dev->bar_addr[1] = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_1);
+    bar1_data = pci_conf_read(ivshmem_dev->bar_addr[1]);
+    uk_pr_info("bar1_data:0x%lx\n", bar1_data);
+
+    ivshmem_dev->bar_addr[2] = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_2);
+	bar2_data = pci_conf_read(ivshmem_dev->bar_addr[2]);
     uk_pr_info("bar2_data:0x%lx\n", bar2_data);
 
-    bar3_addr = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_3);
-	bar3_data = pci_conf_read(bar3_addr);
+    ivshmem_dev->bar_addr[3] = get_config_addr(bus, device, function, PCI_BASE_ADDRESS_3);
+	bar3_data = pci_conf_read(ivshmem_dev->bar_addr[3]);
     uk_pr_info("bar3_data:0x%lx\n", bar3_data);
 
     __u64 ivshmem_addr = (((__u64)bar3_data << 32) | (bar2_data & 0xfffffff0));
@@ -83,7 +105,9 @@ static int ivshmem_pci_add_dev(struct pci_device *pci_dev)
     ivshmem_dev->pdev = pci_dev;
     ivshmem_dev->ivshmem_size = ivshmem_size;
     ivshmem_dev->ivshmem_addr_start = ivshmem_addr;   
+
     
+
     return 0;
 }
 
@@ -107,3 +131,60 @@ static struct pci_driver ivshmem_pci_drv = {
     .add_dev = ivshmem_pci_add_dev
 };
 PCI_REGISTER_DRIVER(&ivshmem_pci_drv);
+
+
+
+static int dev_ivshmem_read(struct device *dev __unused, struct uio *uio,
+			   int flags __unused)
+{
+	size_t count;
+	char *buf;
+
+	buf = uio->uio_iov->iov_base;
+	count = uio->uio_iov->iov_len;
+
+	uk_swrand_fill_buffer(buf, count);
+
+	uio->uio_resid = 0;
+	return 0;
+}
+
+static int dev_ivshmem_write(struct device *dev __unused,
+			    struct uio *uio __unused,
+			    int flags __unused)
+{
+	return EPERM;
+}
+
+static struct devops ivshmem_devops = {
+	.open = dev_noop_open,
+	.close = dev_noop_close,
+	.read = dev_ivshmem_read,
+	.write = dev_ivshmem_write,
+	.ioctl = dev_noop_ioctl,
+};
+
+static struct driver drv_ivshmem = {
+	.devops = &ivshmem_devops,
+	.devsz = 0,
+	.name = DEV_IVSHMEM_NAME
+};
+
+static int devfs_register(struct uk_init_ctx *ictx __unused)
+{
+	int rc;
+
+	uk_pr_info("Register '%s' to devfs\n", DEV_IVSHMEM_NAME);
+
+	/* register /dev/ivshmem */
+	rc = device_create(&drv_ivshmem, DEV_IVSHMEM_NAME, D_CHR, NULL);
+	if (unlikely(rc)) {
+		uk_pr_err("Failed to register '%s' to devfs: %d\n",
+			  DEV_IVSHMEM_NAME, rc);
+		return -rc;
+	}
+
+	return 0;
+}
+
+devfs_initcall(devfs_register);
